37d3c1911bae4daf8fa56a4504e3742ad55c27e5
Victor Odoabuchi <105570056+Alphatechbot@users.noreply.github.com>
2023-09-11 15:57:23 +0100

    Update models.py

--- base/models.py
+++ base/models.py
@@ -8,8 +8,7 @@ class User(AbstractUser):
     bio = models.TextField(null=True)
 
     avatar = models.ImageField(null=True, default="avatar.svg")
-
-    USERNAME_FIELD = 'email'
+    
     REQUIRED_FIELDS = []
 
 
